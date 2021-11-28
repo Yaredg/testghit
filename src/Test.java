392f9733a22a406be0b4507dc0ba95a739512150
yaredg <yaredgidey@gmaile.com>
2021-11-28 18:57:02 -0500

    second task done

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test success");
         System.out.println("new change ");
+        System.out.println("second tasks");
     }
 
 }
