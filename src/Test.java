09c4fcf488ac894e1825f870f7fb68b4e8fe6931
yaredg <yaredgidey@gmaile.com>
2021-12-16 23:38:30 -0500

    test conflict

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
         System.out.println("Test success");
         System.out.println("new change ");
         System.out.println("second tasks");
-
+        System.out.println("Branch 125");
     }
 
 }
