12d34e44b26dd7a5fec1cc4a18d58d1aa5fefcb6
yaredg <yaredgidey@gmaile.com>
2021-12-16 23:47:09 -0500

    new con

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test success");
-        System.out.println("new change ");
+        //System.out.println("Test success");
+        //System.out.println("new change ");
         System.out.println("second tasks");
         System.out.println("Branch 125");
+
     }
 
 }
